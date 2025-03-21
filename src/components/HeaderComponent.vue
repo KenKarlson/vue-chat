a2461a9899ec357b45311ff8264b332e2a3a19bf
Гусь Гитарист <kenkarlson@mail.ru>
2025-03-21 11:37:19 +0300

    Fix header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <h1>Real-Time Chat</h1>
+    <h1 class="header-title">Real-Time Chat (.b)</h1>
   </header>
 </template>
 
@@ -12,9 +12,15 @@ export default {
 
 <style scoped>
 header {
+  margin: 0;
   background-color: #42b983;
   color: white;
   padding: 10px;
   text-align: center;
+}
+.header-title{
+  margin: 0;
+  padding: 0;
+  text-shadow: 2px 2px 2px #007;
 }
 </style>
